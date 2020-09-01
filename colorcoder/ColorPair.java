8ce9c558437affa44e95d8b42feef13cad3a1a3d
BUNARI PRANAY <36372708+Logan111@users.noreply.github.com>
2020-09-01 21:12:20 +0530

    Create ColorPair.java

---
+++ colorcoder/ColorPair.java
@@ -0,0 +1,27 @@
+package colorcoder;
+
+import  colorcoder.MajorColor;
+import  colorcoder.MinorColor;
+
+ class ColorPair {
+        private MajorColor majorColor;
+        private MinorColor minorColor;
+        
+        public ColorPair(MajorColor major, MinorColor minor)
+        {
+            majorColor = major;
+            minorColor = minor;
+        }
+        public MajorColor getMajor() {
+            return majorColor;
+        }
+        public MinorColor getMinor() {
+            return minorColor;
+        }
+        String ToString() {
+            String colorPairStr = Methods.MajorColorNames[majorColor.getIndex()];
+            colorPairStr += " ";
+            colorPairStr += Methods.MinorColorNames[minorColor.getIndex()];
+            return colorPairStr;
+        }
+    };
