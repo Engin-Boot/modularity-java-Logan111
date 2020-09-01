b364e777b56ebf6e88f99af0ae5462c6b9c7f32d
BUNARI PRANAY <36372708+Logan111@users.noreply.github.com>
2020-09-01 21:15:50 +0530

    Create Testers.java

---
+++ colorcoder/Testers.java
@@ -0,0 +1,33 @@
+package colorcoder;
+
+public class Testers 
+{
+    static void Printmanual_by_StrConcat()
+    {
+        int total_entries  = Methods.numberOfMajorColors * Methods.numberOfMinorColors;
+        String manual_string = "";
+        for(int pair_num = 1; pair_num <= total_entries; pair_num++)
+        {
+            ColorPair paircolor =  Methods.GetColorFromPairNumber(pair_num);
+            manual_string = manual_string + pair_num + " - " + paircolor.getMajor() + " , " + paircolor.getMinor() + "\n";
+            
+        }
+         System.out.println(manual_string);
+    }
+    
+    static void testNumberToPair(int pairNumber,MajorColor expectedMajor,MinorColor expectedMinor)
+    {
+        ColorPair colorPair = Methods.GetColorFromPairNumber(pairNumber);
+        System.out.println("Got pair " + colorPair.ToString());
+        assert(colorPair.getMajor() == expectedMajor);
+        assert(colorPair.getMinor() == expectedMinor);
+    }
+
+    static void testPairToNumber(MajorColor major,MinorColor minor,int expectedPairNumber)
+    {
+        int pairNumber = Methods.GetPairNumberFromColor(major, minor);
+        System.out.println("Got pair number " + pairNumber);
+        assert(pairNumber == expectedPairNumber);
+    }
+    
+}
